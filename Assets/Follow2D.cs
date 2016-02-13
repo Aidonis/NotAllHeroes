88d4cb99638a363826715ebf22842cf82f92e97e
unknown <ryan.floyd@SEA-8E40F2.sea.aie.edu>
2016-02-12 17:35:39 -0800

    first push

---
+++ Assets/Follow2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow2D : MonoBehaviour {
+
+    public Vector2 target;
+    public float speed = 0;
+    [SerializeField]
+    private Vector2 thisSpot;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thisSpot = new Vector2(transform.position.x, transform.position.z);
+
+        //
+
+        Vector2 movement = (target - thisSpot).normalized;
+        
+        if ((target - thisSpot).magnitude < speed)
+        {
+            transform.position = new Vector3(target.x, transform.position.y, target.y);
+        }
+        else
+        {
+            transform.position += new Vector3(movement.x * speed, 0, movement.y * speed);
+        }
+	}
+}
---
+++ Assets/Follow3D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow3D : MonoBehaviour {
+
+    public Vector3 target;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = (target - transform.position).normalized;
+
+        if ((target - transform.position).magnitude < speed)
+        {
+            transform.position = target;
+        }
+        else
+        {
+            transform.position += movement * speed;
+        }
+	}
+}
